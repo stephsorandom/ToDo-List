6893ab45a3c51582a972717f21da81a31d0d5a32
Stephanie Mitchell <stephsorandom@gmail.com>
2022-01-21 17:53:41 -0500

    trying to add feature..when list items clicked, shows red

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const userList = document.querySelector('.name-list li');
+const userList = document.querySelectorAll('.name-list li');
 const listInput = document.querySelector('.list-input');
 const addBtn = document.querySelector('.add-btn');
+const deleteBtn = document.querySelector('.delete-btn');
 
 
 addBtn.addEventListener('click', () => {
@@ -8,9 +9,21 @@ addBtn.addEventListener('click', () => {
     const newLi = document.createElement('LI')
     const liContent = document.createTextNode(listInput.value)
     ///Add the input value inside new li
-    newLi.appendChild(liContent)
+    newLi.appendChild(liContent);
     //Attach li to user list
-    userList.appendChild(newLi)
+    userList.appendChild(newLi);
+    //Append new LI to end of list
+    // liContent.after(liContent.lastElementChild)
 })
+ for (user of userList) {
+     user.addEventListener('click', () => {
+        console.log(user)
+     });
+ }
+// liContent.addEventListener('click', () => {
 
- 
+// })
+
+//  deleteBtn.addEventListener('click', () => {
+    
+//  })
